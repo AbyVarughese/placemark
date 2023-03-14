7462e53bc1683ebd6b366c3676a3de5ab1faaca9
Aby Varughese <abyasnu@gmail.com>
2023-03-14 10:11:51 +0000

    Deploying changes

--- src/server.js
+++ src/server.js
@@ -32,9 +32,11 @@ const swaggerOptions = {
 };
 
 async function init() {
+  //const server = Hapi.server({
+  //  port: 3000,
+   // host: "localhost",
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
   });
 
   await server.register(Inert);
