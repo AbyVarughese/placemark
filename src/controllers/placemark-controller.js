fb79de7d301e0796e1e634f39838a3126bfe2dc8
Aby Varughese <abyasnu@gmail.com>
2023-05-19 18:38:15 +0100

    public map

--- src/controllers/placemark-controller.js
+++ src/controllers/placemark-controller.js
@@ -21,9 +21,19 @@ export const placemarkController = {
         handler: async function (request, h) {
             const loggedInUser = request.auth.credentials;
             const placemarks = await db.placemarkStore.getPublicPlacemarks();
+            let markers = "";
+            for (let i = 0; i < placemarks.length; i++) {
+                markers += "L.marker(["+(placemarks[i].latitude||"").toString()+", "+(placemarks[i].longitude|"").toString()+"]).addTo(map);\r\n";
+            }
+            let mapview = null;
+            if (placemarks.length > 0) {
+                mapview = ".setView(["+(placemarks[0].latitude||"").toString()+", "+(placemarks[0].longitude||"").toString()+"], 6.4)";
+            }
             const viewData = {
                 placemarks: placemarks,
                 loggedInUser: loggedInUser,
+                markers: markers,
+                mapview: mapview,
             };
             return h.view("public-view", viewData);
         },
